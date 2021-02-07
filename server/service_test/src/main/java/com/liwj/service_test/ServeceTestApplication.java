49d56c9f5d487cc1c930e93b6cfeda15f5f43278
liwj <liwenji-e@outlook.com>
2021-02-07 16:24:12 +0800

    项目结构处理

---
+++ server/service_test/src/main/java/com/liwj/service_test/ServeceTestApplication.java
@@ -0,0 +1,13 @@
+package com.liwj.service_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeceTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServeceTestApplication.class, args);
+    }
+
+}
---
+++ server/service_test/src/test/java/com/liwj/service_test/ServeceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liwj.service_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeceTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
